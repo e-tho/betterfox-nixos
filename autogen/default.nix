b5d82ebf3779fe1d986b991f6476f6c923782a21
Etho Ledroy <etho.ledroy@pm.me>
2024-02-05 15:20:47 +0100

    Add autogen content.

---
+++ autogen/default.nix
@@ -0,0 +1,14 @@
+{
+  "119.0" = builtins.readFile ./119.0.json;
+  "118.0" = builtins.readFile ./118.0.json;
+  "117.0" = builtins.readFile ./117.0.json;
+  "116.1" = builtins.readFile ./116.1.json;
+  "116.0" = builtins.readFile ./116.0.json;
+  "115.0" = builtins.readFile ./115.0.json;
+  "112.0" = builtins.readFile ./112.0.json;
+  "111.0" = builtins.readFile ./111.0.json;
+  "110.0" = builtins.readFile ./110.0.json;
+  "109.0" = builtins.readFile ./109.0.json;
+  "108.0" = builtins.readFile ./108.0.json;
+  "107.0" = builtins.readFile ./107.0.json;
+}
